b390c1167fe242009d1344899271e78617b81670
Marek Slawinski <marek.slawinski@live.com>
2018-03-06 17:33:36 +0100

    Add header, side-nav, main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule
